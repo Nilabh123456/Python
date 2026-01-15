947c75146eefd861ac238c4877d4a4ccf0ef3207
Nilabh123456 <nilabhsharma44@gmail.com>
2026-01-15 13:26:23 +0530

    Add files via upload

---
+++ SI.py
@@ -0,0 +1,7 @@
+#start
+amount =float(input("Enter the values:"))
+rate=float(input("Enter the values:"))
+time=float(input("Enter the value:"))
+SI= amount*rate*time/100
+print("SI",SI)
+                  
